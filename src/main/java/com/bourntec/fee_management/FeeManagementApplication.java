ef8448d7a64e518ff1f03650d82180d933c2fcad
Honeymol Varghese <honeyvarghesegie@gmail.com>
2021-06-24 12:09:13 +0530

    initial commit

---
+++ src/main/java/com/bourntec/fee_management/FeeManagementApplication.java
@@ -0,0 +1,14 @@
+package com.bourntec.fee_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeeManagementApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/bourntec/fee_management/FeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bourntec.fee_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
